db64512092e5f931e1d97ea50277ae31ff5e5d20
metisandys <andym1994@163.com>
2016-09-13 17:07:52 +0800

    提交index

--- application/views/index.php
+++ application/views/index.php
@@ -3,10 +3,26 @@
 <head>
     <meta charset="UTF-8">
     <title>Document</title>
+    <meta name="viewport"
+          content="width=device-width, initial-scale=1.0">
+    <base href="<?php echo site_url();?>">
+    <link rel="stylesheet" href="../../css/style.css">
+    <link rel="stylesheet" href="../../css/index.css">
+    <script>
+        window.addEventListener("load", function(){
+            setTimeout(hideURLbar, 0);
+        }, false);
+        function hideURLbar(){
+            window.scrollTo(0,1);
+        }
+    </script>
 </head>
 <body>
-    haha
-
-    hoho
+    <?php include 'header.php';?>
+    <div id="banner"></div>
+    <div id="workers"></div>
+    <div id="services"></div>
+    <div id="meet"></div>
+    <footer id="footer"></footer>
 </body>
 </html>
