0d18f77123738ef97f806ea91852448ca76ec5bf
metisandys <andym1994@163.com>
2016-09-13 21:35:13 +0800

    网站更新

---
+++ application/views/header.php
@@ -0,0 +1,24 @@
+<header id="header">
+    <div class="wrapper">
+        <a href="#">
+            <img src="../../img/logo.png" alt="">
+        </a>
+        <div id="header-menu">
+            <a href="#header-nav" id="btn-show"><span class="glyphicon glyphicon-align-justify"></span></a>
+            <ul id="header-nav">
+                <li><a href="#">Home</a></li>
+                <li><a href="#">Services</a></li>
+                <li><a href="#">Portfolio</a></li>
+                <li><a href="#">Blog</a></li>
+                <li><a href="#">Contact</a></li>
+                <li class="search">
+                    <div id="search-box">
+                        <input type="search" id="search-input" placeholder="Enter your search term..">
+                        <button type="submit" id="search-btn"></button>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </div>
+
+</header>
--- application/views/index.php
+++ application/views/index.php
@@ -2,27 +2,28 @@
 <html lang="en">
 <head>
     <meta charset="UTF-8">
-    <title>Document</title>
+    <title>首页</title>
     <meta name="viewport"
           content="width=device-width, initial-scale=1.0">
-    <base href="<?php echo site_url();?>">
+<!--    <base href="--><?php //echo site_url();?><!--">-->
     <link rel="stylesheet" href="../../css/style.css">
     <link rel="stylesheet" href="../../css/index.css">
     <script>
-        window.addEventListener("load", function(){
+        window.addEventListener("load", function () {
             setTimeout(hideURLbar, 0);
         }, false);
-        function hideURLbar(){
-            window.scrollTo(0,1);
+        function hideURLbar() {
+            window.scrollTo(0, 1);
         }
     </script>
 </head>
 <body>
     <?php include 'header.php';?>
     <div id="banner"></div>
-    <div id="workers"></div>
+    <div id="works"></div>
     <div id="services"></div>
     <div id="meet"></div>
     <footer id="footer"></footer>
+    <script src="../../js/require.js" data-main="../../js/index"></script>
 </body>
 </html>
